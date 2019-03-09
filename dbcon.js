b9946e651f34b3960f3016f5ebb81cfe3ed53e9d
Sam <samtone93@gmail.com>
2019-03-09 00:01:31 -0600

    Add files via upload

--- dbcon.js
+++ dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_lemuso',
-  password        : '5813',
-  database        : 'cs340_lemuso'
+  user            : 'cs340_tones',
+  password        : '5288',
+  database        : 'cs340_tones'
 });
 module.exports.pool = pool;
