0fac48e4952103fdb8b0196b6ddb19649d1f5ca7
lemuso <lemuso@oregonstate.edu>
2019-03-20 00:38:04 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -41,6 +41,20 @@ function getCharacters(res, mysql, context, complete){
   });
 }
 
+function filterCharacters(sql,res, mysql, context, complete){
+  mysql.pool.query(sql,function(error, results, fields){
+    if(error){
+      res.write(JSON.stringify(error));
+      res.end();
+    }
+      context.characters = results;
+      complete();
+    });
+
+
+}
+
+
 app.get('/characters',function(req,res, next){
   var context = {};
   var mysql = req.app.get('mysql');
@@ -60,16 +74,33 @@ app.get('/characters',function(req,res, next){
   }
 }); 
 
-/*
 
-app.post('/filter' ,function(req,res, next){
- console.log(req.body.)
-    console.log(req.body)
+app.post('/filter', function(req, res){
+    //console.log(req.body.filter_this); 
+    //console.log(req.body); 
+    var callbackCount = 0; 
+    var context = {};
+    var mysql = req.app.get('mysql');
+    var a = "SELECT a.character_Id, a.character_name, d.character_culture,b.mothers_house, c.fathers_house, e.allegiance, a.is_alive FROM ( SELECT character_Id, character_name, character_culture,mothers_house, fathers_house, allegiance, is_alive FROM characters WHERE ";
+    var b = ") a LEFT JOIN ( SELECT house_id, houses_name AS mothers_house FROM houses ) b ON a.mothers_house =  b.house_id  LEFT JOIN ( SELECT house_id, houses_name AS fathers_house FROM houses ) c ON a.fathers_house =  c.house_id LEFT JOIN (SELECT culture_id, culture_name AS character_culture FROM cultures) d ON a.character_culture = d.culture_id LEFT JOIN (SELECT house_id, houses_name AS allegiance FROM houses) e ON a.allegiance = e.house_id ORDER BY a.character_name";
+    var sql = a + req.body.filter_by + b; 
 
+    //console.log(sql); 
 
-}); 
+    var inserts = [req.body.filter_by];
 
-*/
+    filterCharacters (sql,res, mysql, context, complete);
+
+   function complete(){
+            callbackCount++;
+            if(callbackCount >= 1){
+                //console.log(context); 
+                res.render('filter', context);
+         }
+  }
+});
+
+  
 
 function getCultures(res, mysql, context, complete){
   mysql.pool.query("SELECT * FROM cultures ORDER BY culture_name ", function(error, results, fields){
@@ -120,10 +151,11 @@ app.get('/houses',function(req,res, next){
   var mysql = req.app.get('mysql');
   var callbackCount = 0; 
   getHouses(res, mysql, context, complete);
+  getCharacters(res, mysql, context, complete);
 
    function complete(){
             callbackCount++;
-            if(callbackCount >= 1){
+            if(callbackCount >= 2){
                 //console.log(context); 
                 res.render('houses', context);
   }
@@ -151,35 +183,22 @@ app.get('/seats',function(req,res, next){
   var mysql = req.app.get('mysql');
   var callbackCount = 0; 
   getSeats(res, mysql, context, complete);
+  getCharacters(res, mysql, context, complete);
+  getHouses(res, mysql, context, complete);
 
    function complete(){
             callbackCount++;
-            if(callbackCount >= 1){
+            if(callbackCount >= 3){
                 //console.log(context); 
                 res.render('seats', context);
   }
 
   }
 });  
-/*
-app.post('/characters',function(req,res,next){
-  var context = {};
-  mysql.pool.query("INSERT INTO characters (`character_name`, character_culture, mothers_house, fathers_house, allegiance, is_alive) VALUES (?,?,?,?,?,?)", [req.query.c], function(err, result){
-    if(err){
-      next(err);
-      return;
-    }
-
-    res.redirect('/characters');
-
-  });
-})
-
-*/
 
 app.post('/characters', function(req, res){
-    console.log(req.body.character_name)
-    console.log(req.body)
+    console.log(req.body.character_name);
+    console.log(req.body);
     var mysql = req.app.get('mysql');
     var sql = "INSERT INTO characters (character_name, character_culture, mothers_house, fathers_house, allegiance, is_alive) VALUES (?,?,?,?,?,?)";
     var inserts = [req.body.character_name, req.body.character_culture, req.body.mothers_house, req.body.fathers_house, req.body.allegiance, req.body.is_alive];
@@ -382,120 +401,9 @@ app.post('/houses/delete/:id', function(req, res){
   });
 });
 
-//app.get('/characters',function(req,res){
-//  res.render('characters'); 
-//});
-
-//app.get('/cultures',function(req,res){
-//  res.render('cultures'); 
-//});
-
-//app.get('/houses',function(req,res){
-//  res.render('houses'); 
-//});
-
-//app.get('/seats',function(req,res){
-//  res.render('seats'); 
-//});
-
 function isAliveHelper(stuff){
     console.log(stuff); 
 }
-
-
-
-/*
-app.get('/',function(req,res,next){
-  var context = {};
-  mysql.pool.query('SELECT * FROM todo', function(err, rows, fields){
-    if(err){
-      next(err);
-      return;
-    }
-    context.results = JSON.stringify(rows);
-    res.render('home', context);
-  });
-});
-
-app.get('/insert',function(req,res,next){
-  var context = {};
-  mysql.pool.query("INSERT INTO todo (`name`) VALUES (?)", [req.query.c], function(err, result){
-    if(err){
-      next(err);
-      return;
-    }
-    context.results = "Inserted id " + result.insertId;
-    res.render('home',context);
-  });
-});
-
-app.get('/delete',function(req,res,next){
-  var context = {};
-  mysql.pool.query("DELETE FROM todo WHERE id=?", [req.query.id], function(err, result){
-    if(err){
-      next(err);
-      return;
-    }
-    context.results = "Deleted " + result.changedRows + " rows.";
-    res.render('home',context);
-  });
-});
-
-
-///simple-update?id=2&name=The+Task&done=false&due=2015-12-5
-app.get('/simple-update',function(req,res,next){
-  var context = {};
-  mysql.pool.query("UPDATE todo SET name=?, done=?, due=? WHERE id=? ",
-    [req.query.name, req.query.done, req.query.due, req.query.id],
-    function(err, result){
-    if(err){
-      next(err);
-      return;
-    }
-    context.results = "Updated " + result.changedRows + " rows.";
-    res.render('home',context);
-  });
-});
-
-///safe-update?id=1&name=The+Task&done=false
-app.get('/safe-update',function(req,res,next){
-  var context = {};
-  mysql.pool.query("SELECT * FROM todo WHERE id=?", [req.query.id], function(err, result){
-    if(err){
-      next(err);
-      return;
-    }
-    if(result.length == 1){
-      var curVals = result[0];
-      mysql.pool.query("UPDATE todo SET name=?, done=?, due=? WHERE id=? ",
-        [req.query.name || curVals.name, req.query.done || curVals.done, req.query.due || curVals.due, req.query.id],
-        function(err, result){
-        if(err){
-          next(err);
-          return;
-        }
-        context.results = "Updated " + result.changedRows + " rows.";
-        res.render('home',context);
-      });
-    }
-  });
-});
-
-app.get('/reset-table',function(req,res,next){
-  var context = {};
-  mysql.pool.query("DROP TABLE IF EXISTS todo", function(err){
-    var createString = "CREATE TABLE todo(" +
-    "id INT PRIMARY KEY AUTO_INCREMENT," +
-    "name VARCHAR(255) NOT NULL," +
-    "done BOOLEAN," +
-    "due DATE)";
-    mysql.pool.query(createString, function(err){
-      context.results = "Table reset";
-      res.render('home',context);
-    })
-  });
-});
-*/
 
 app.use(function(req,res){
   res.status(404);
